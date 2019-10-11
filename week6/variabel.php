02dd34af65be1774ce462828d1a83eb4e7db27a2
dewaayupw <dewaayuputuwidiasari1@gmail.com>
2019-10-11 10:34:42 +0700

    prak6

---
+++ week6/variabel.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title> contoh skrip PHP</title>
+</head>
+<body>
+    <?php
+    $nama = "Kim Taehyung";
+    echo "$nama berkala \"save me save me\" ";
+    ?>
+</body>
+</html>
